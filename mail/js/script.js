ddacdd18073eb947309da78285f97446339419d3
tahakyo <ghazouani1988@email.com>
2022-04-18 18:06:54 +0200

    js-mail

---
+++ mail/js/script.js
@@ -0,0 +1,15 @@
+// Lista delle email registrate
+const emailList = ['ghazgt@hotmail.fr','ghazouani1988@gmail.com'];
+console.log(emailList);
+// chiedo all'utente di inserire una mail
+const userMail = prompt('inserici qui la tua mail');
+console.log(userMail);
+// controllo se la mail è nella list o no
+mailFound = false;
+
+for (let i = 0; i < emailList.length; i++) {
+    if (userMail === emailList[i]) {
+        mailFound = true;
+    }
+}
+console.log(mailFound);
